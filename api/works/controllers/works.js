15c519ad883c88c19ad7017037345a5441525d76
grupodixis <grupodixis@gmail.com>
2021-10-14 20:11:23 +0200

    update workOrder Recursive

--- api/works/controllers/works.js
+++ api/works/controllers/works.js
@@ -5,4 +5,23 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+
+    async worksOrder(ctx){
+        let works = await strapi.services.works.find(ctx.query)
+        let myMap ={}
+        let tree =[]
+        works.forEach(work => {
+            myMap[work.id] = work
+            myMap[work.id].childs = []
+            if (myMap[work.id].father == null){
+                tree.push(myMap[work.id])
+            }else{
+                if (typeof myMap[work.father.id] === "object") {
+                    myMap[work.father.id].childs.push(work);
+                  }
+            }
+        }); 
+        return tree
+    }
+};
